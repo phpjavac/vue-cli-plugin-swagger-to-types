246c880a6d4774cb4ebfb0332aa17dc0c9589bac
孙福聪 <magiccc@sunfucongdeMacBook-Pro.local>
2020-08-04 09:55:22 +0800

    初始提交

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # vue-cli-plugin-swagger-to-types
 自动从swagger生成inteface
+
+```JavaScript
+// 1.安装插件
+  vue add swagger-to-types
+
+// 2.vue.config.js中设置配置
+  pluginOptions: {
+    toTypes: {
+      host: "http://192.168.1.1/demo-web/v2/api-docs", // ftp地址
+      TypesPath: "./types/api.d.ts" // 项目地址
+     }
+  },
+
+// 3.package.json中 build的命令修改为
+  vue-cli-service build --no-clean && vue-cli-service getTypes
+
+  ```
---
+++ index.js
@@ -0,0 +1,30 @@
+const axios = require("axios")
+const fs = require("fs")
+const { readFileSync } = require("fs");
+const swaggerToTS = require("@manifoldco/swagger-to-ts").default;
+module.exports = (api, projectOptions) => {
+  api.registerCommand('getTypes', () => {
+    if (!projectOptions.pluginOptions || !projectOptions.pluginOptions.toTypes) {
+      console.log("未设置生成Types配置")
+      return;
+    }
+    if (!projectOptions.pluginOptions.toTypes.host || !projectOptions.pluginOptions.toTypes.TypesPath) {
+      console.log("未设置Types地址或输出路径")
+      return;
+    }
+
+
+    const url = projectOptions.pluginOptions.toTypes.host
+
+    axios.get(url).then(res => {
+      // console.log(res.data);
+      const input = res.data; // Input can be any JS object (OpenAPI format)
+      const output = swaggerToTS(input); // Outputs TypeScript defs as a string (to be parsed, or written to a file)
+      fs.writeFile(projectOptions.pluginOptions.toTypes.TypesPath, output, { encoding: 'utf8' }, err => {
+        if (err) throw err;
+        console.log('生成Types成功！');
+      })
+    })
+
+  })
+}
